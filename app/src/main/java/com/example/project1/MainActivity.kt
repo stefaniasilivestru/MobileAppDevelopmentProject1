79eafa9915c0127aa2e1b992a40630d0c09ae080
Silivestru Stefania <stefania.silivestru4@gmail.com>
2024-02-29 15:51:05 +0100

    Start new design to implement our project idea

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,161 +1,57 @@
 package com.example.project1
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.view.marginRight
-import java.io.File
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.project1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), LocationListener {
-    private val TAG1 = "buttonMain"
-    private val TAG2 = "activityCreated"
-    private val TAG3 = "location"
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var locationManager : LocationManager
-    private lateinit var latestLocation: Location
-    private val locationPermissionCode = 2
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        Log.d(TAG2, "onCreate: The main activity is being created.");
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button: Button = findViewById(R.id.buttonMain)
-        button.setOnClickListener {
-            Log.d(TAG1, "Button from Main Activity was clicked")
-            val intent = Intent(this, SecondActivity::class.java)
-            val bundle = Bundle()
-            bundle.putParcelable("location", latestLocation)
-            intent.putExtra("locationBundle", bundle)
-            startActivity(intent)
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
-
-        val buttonToMap : Button = findViewById(R.id.buttonToMap)
-        buttonToMap.setOnClickListener {
-            if (latestLocation != null) {
-                val intent = Intent(this, OpenStreetMapActivity::class.java)
-                val bundle = Bundle()
-                bundle.putParcelable("location", latestLocation)
-                intent.putExtra("locationBundle", bundle)
-                startActivity(intent)
-            } else{
-                Log.e(TAG3, "Location not set yet.")
-            }
-        }
-
-        val buttonRegister : Button = findViewById(R.id.buttonRegister)
-        buttonRegister.setOnClickListener {
-            val userIdentifier = getUserIdentifier()
-            if (userIdentifier == null) {
-                askForUserIdentifier()
-            } else {
-                Toast.makeText(this, "You are register as $userIdentifier!", Toast.LENGTH_SHORT).show()
-                val textView : TextView = findViewById(R.id.textViewRegister)
-                textView.text = "You are register as $userIdentifier!"
-            }
-        }
-
-        // Get the location
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
-                locationPermissionCode
-            )
-        } else {
-            // The location is updated every 5000 milliseconds (or 5 seconds) and/or if the device moves more than 5 meters,
-            // whichever happens first
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
-        }
-
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == locationPermissionCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
-                }
-            }
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 
-    override fun onLocationChanged(location: Location) {
-        val textView : TextView = findViewById(R.id.mainTextView)
-        textView.text = "Your current location is set at:" +
-                "\nLatitude: ${location.latitude}, Longitude: ${location.longitude}"
-        textView.gravity = Gravity.CENTER
-        Toast.makeText(this, "Location updated! [${location.latitude}, ${location.longitude}]", Toast.LENGTH_SHORT).show()
-        saveCoordinatesToFile(location.latitude, location.longitude)
-        latestLocation = location // Initialize latestLocation
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-    private fun askForUserIdentifier() {
-        val input = EditText(this)
-        AlertDialog.Builder(this)
-            .setTitle("Enter your name")
-            .setIcon(R.mipmap.ic_launcher)
-            .setView(input)
-            .setPositiveButton("OK") { _, _ ->
-                val name = input.text.toString()
-                Log.d("User", "The user's name is $name")
-                if (name.isNotBlank()) {
-                    saveUserIdentifier(name)
-                    Toast.makeText(this, "Welcome, $name!", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Please enter a valid name.", Toast.LENGTH_SHORT).show()
-                }
-            }
-            .setNegativeButton("Cancel", null)
-            .show()
-    }
-
-    private fun getUserIdentifier() : String? {
-        val sharedPreferences = this.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
-        return sharedPreferences.getString("userIdentifier", null)
-    }
-
-    private fun saveUserIdentifier(name: String) {
-        val sharedPreferences = this.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
-        sharedPreferences.edit().apply {
-            putString("userIdentifier", name)
-            apply()
-        }
-    }
-
-    private fun saveCoordinatesToFile(latitude: Double, longitutde: Double) {
-        val fileName = "coordinates.txt"
-        val file = File(filesDir, fileName)
-        val time = System.currentTimeMillis()
-        file.appendText("$time: [$latitude, $longitutde]\n")
-
-    }
-
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-    override fun onProviderEnabled(provider: String) {}
-    override fun onProviderDisabled(provider: String) {}
 }
